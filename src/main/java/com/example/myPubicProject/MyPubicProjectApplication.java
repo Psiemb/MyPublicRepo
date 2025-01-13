7a9b8a63a3e8b42854a80fede4483369fe71d78d
Psiemb <email@example.com>
2025-01-13 14:11:04 +0100

    My public Repo

---
+++ src/main/java/com/example/myPubicProject/MyPubicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.myPubicProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPubicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPubicProjectApplication.class, args);
+	}
+
+}
